9989a6d0e9dd251ff3a607674e422f148b99bca2
Elaiza Bolislis <zbolislis@gmail.com>
2024-11-11 02:47:38 +0800

    Removed do_eval (for now)

--- run_ld.sh
+++ run_ld.sh
@@ -22,9 +22,7 @@
 /mnt/c/Users/ezbol/OneDrive/Desktop/thesis-it_test-model/thesis-env/Scripts/python.exe run_ld.py \
   --task_name fc \
   --do_train \
-  --do_eval \
   --do_lower_case \
-  --adv_training \
   --data_dir ./datasets/ \
   --bert_model bert-base-multilingual-cased \
   --max_seq_length 128 \
@@ -33,4 +31,4 @@
   --num_k 1 \
   --num_self_train 1 \
   --num_train_epochs 3.0 \
-  --output_dir /tmp/mld_ja_ld_output/
+  --output_dir /tmp/mbert-sl_output/
